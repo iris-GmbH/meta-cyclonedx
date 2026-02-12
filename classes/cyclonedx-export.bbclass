iris-GmbH/meta-cyclonedx

commit d3a78c04785e14de3e1ea825d1e09fe156b143ce

Limit SBOM content to packages added to the rootfs
We use open embedded list_installed_packages function to filter for
runtime packages added to the rootfs. Additionally we now use sstate
caching mechanism to speed up cyclonedx sbom generation on rebuilds.

Fixes https://github.com/savoirfairelinux/meta-cyclonedx/issues/9

Co-authored-by: Jasper Orschulko <jasper@fancydomain.eu>
Signed-off-by: Marcel Plumbohm <marcel.plumbohm@iris-sensing.com>
Signed-off-by: Jasper Orschulko <jasper@fancydomain.eu>

classes/cyclonedx-export.bbclass
@@ -7,17 +7,25 @@
 CVE_PRODUCT ??= "${BPN}"
 CVE_VERSION ??= "${PV}"
 
+CYCLONEDX_RUNTIME_PACKAGES_ONLY ??= "1"
+
 CYCLONEDX_EXPORT_DIR ??= "${DEPLOY_DIR}/cyclonedx-export"
 CYCLONEDX_EXPORT_SBOM ??= "${CYCLONEDX_EXPORT_DIR}/bom.json"
 CYCLONEDX_EXPORT_VEX ??= "${CYCLONEDX_EXPORT_DIR}/vex.json"
-CYCLONEDX_EXPORT_TMP ??= "${TMPDIR}/cyclonedx-export"
-CYCLONEDX_EXPORT_LOCK ??= "${CYCLONEDX_EXPORT_TMP}/bom.lock"
+CYCLONEDX_TMP_WORK_DIR ??= "${WORKDIR}/cyclonedx"
+CYCLONEDX_TMP_PN_LIST = "${CYCLONEDX_TMP_WORK_DIR}/pn-list.json"
+CYCLONEDX_WORK_DIR_ROOT ??= "${TMPDIR}/cyclonedx"
+CYCLONEDX_WORK_DIR = "${CYCLONEDX_WORK_DIR_ROOT}/${PN}"
+CYCLONEDX_WORK_DIR_PN_LIST = "${CYCLONEDX_WORK_DIR}/pn-list.json"
+
+# We need to add the sbom serial number to the list of vulnerabilites for each recipe but
+# don't know it until after we generate the sbom export header file
+CYCLONEDX_SBOM_SERIAL_PLACEHOLDER = "<SBOM_SERIAL>"
 
 # resolve CVE_CHECK_IGNORE and CVE_STATUS_GROUPS,
 # taken from https://git.yoctoproject.org/poky/commit/meta/classes/cve-check.bbclass?id=be9883a92bad0fe4c1e9c7302c93dea4ac680f8c
 # SPDX-License-Identifier: MIT
-# Copyright (C) 2023 Andrej Valek <andrej.valek@siemens.com>
-
+# Copyright OpenEmbedded Contributors
 python () {
     # Fallback all CVEs from CVE_CHECK_IGNORE to CVE_STATUS
     cve_check_ignore = d.getVar("CVE_CHECK_IGNORE")
@@ -36,82 +44,71 @@ python () {
             bb.warn("CVE_STATUS_GROUPS contains undefined variable %s" % cve_status_group)
 }
 
-python do_cyclonedx_init() {
-    import uuid
-    from datetime import datetime, timezone
-
-    timestamp = datetime.now(timezone.utc).isoformat()
-    sbom_dir = d.getVar("CYCLONEDX_EXPORT_DIR")
-    bb.debug(2, "Creating cyclonedx directory: %s" % sbom_dir)
-    bb.utils.mkdirhier(sbom_dir)
-
-    # Generate unique serial numbers for sbom and vex document
-    sbom_serial_number = str(uuid.uuid4())
-    vex_serial_number = str(uuid.uuid4())
-
-    bb.debug(2, f"Creating empty sbom file with serial number {sbom_serial_number}")
-    write_json(d.getVar("CYCLONEDX_EXPORT_SBOM"), {
-        "bomFormat": "CycloneDX",
-        "specVersion": "1.4",
-        "serialNumber": f"urn:uuid:{sbom_serial_number}",
-        "version": 1,
-        "metadata": {
-            "timestamp": timestamp,
-            "tools": [{"name": "yocto"}]
-        },
-        "components": []
-    })
-
-    bb.debug(2, f"Creating empty vex file with serial number {vex_serial_number}")
-    write_json(d.getVar("CYCLONEDX_EXPORT_VEX"), {
-        "bomFormat": "CycloneDX",
-        "specVersion": "1.4",
-        "serialNumber": f"urn:uuid:{vex_serial_number}",
-        "version": 1,
-        "metadata": {
-            "timestamp": timestamp,
-            "tools": [{"name": "yocto"}]
-        },
-        "vulnerabilities": []
-    })
+# Clean out work folder to avoid leftovers from previous builds when including build-time package
+# information and a recipe was removed from the dependency list. (CYCLONEDX_RUNTIME_PACKAGES_ONLY set to 0)
+python clean_cyclonedx_work_folder() {
+    bb.note(f"Cleaning cyclonedx work folder {d.getVar('CYCLONEDX_WORK_DIR_ROOT')}")
 }
-addhandler do_cyclonedx_init
-do_cyclonedx_init[eventmask] = "bb.event.BuildStarted"
+clean_cyclonedx_work_folder[cleandirs] = "${CYCLONEDX_WORK_DIR_ROOT}"
+addhandler clean_cyclonedx_work_folder
+clean_cyclonedx_work_folder[eventmask] = "bb.event.BuildStarted"
 
 python do_cyclonedx_package_collect() {
+    """
+    Collect package information and CVE data from all packages built for the target architecture.
+    """
     from oe.cve_check import decode_cve_status
+
+    pn = d.getVar("PN")
 
     # ignore non-target packages
     for ignored_suffix in (d.getVar("SPECIAL_PKGSUFFIX") or "").split():
-        if d.getVar("PN").endswith(ignored_suffix):
+        if pn.endswith(ignored_suffix):
             return
 
-    # load the bom
+    # get all CVE product names and version from the recipe
     name = d.getVar("CVE_PRODUCT")
     version = d.getVar("CVE_VERSION")
-    sbom = read_json(d.getVar("CYCLONEDX_EXPORT_SBOM"))
-    # extract the sbom serial number without "urn:uuid:" prefix
-    # (avoid using builtin str.removeprefix function as Python >= 3.9 required)
-    sbom_serial_number = sbom["serialNumber"][len("urn:uuid:"):]
-    vex = read_json(d.getVar("CYCLONEDX_EXPORT_VEX"))
 
+    # We create and populate a per-recipe partial sbom which will be added to the sstate cache
+    pn_list = {}
+    pn_list["pkgs"] = []
+    cves = []
+    # append all defined package names for recipe to pn_list pkgs
     for pkg in generate_packages_list(name, version):
-        if not next((c for c in sbom["components"] if c["cpe"] == pkg["cpe"]), None):
-            sbom["components"].append(pkg)
+        if not next((c for c in pn_list["pkgs"] if c["cpe"] == pkg["cpe"]), None):
+            pn_list["pkgs"].append(pkg)
             bom_ref = pkg["bom-ref"]
 
+            # append any cve status within recipe to pn_list cves
             for cve in (d.getVarFlags("CVE_STATUS") or {}):
-                append_to_vex_vulnerabilities(d, vex, cve, sbom_serial_number, bom_ref)
-    
-    # write it back to the deploy directory
-    write_json(d.getVar("CYCLONEDX_EXPORT_SBOM"), sbom)
-    write_json(d.getVar("CYCLONEDX_EXPORT_VEX"), vex)
+                append_to_vex(d, cve, cves, bom_ref)
+
+    pn_list["cves"] = cves
+
+    # write partial sbom to the recipes work folder
+    write_json(d.getVar("CYCLONEDX_TMP_PN_LIST"), pn_list)
 }
 
 addtask do_cyclonedx_package_collect before do_build
-do_cyclonedx_package_collect[nostamp] = "1"
-do_cyclonedx_package_collect[lockfiles] += "${CYCLONEDX_EXPORT_LOCK}"
-do_rootfs[recrdeptask] += "do_cyclonedx_package_collect"
+do_cyclonedx_package_collect[cleandirs] = "${CYCLONEDX_TMP_WORK_DIR}"
+
+# Utilizing shared state for output caching
+# see https://docs.yoctoproject.org/overview-manual/concepts.html#shared-state
+SSTATETASKS += "do_populate_cyclonedx"
+do_populate_cyclonedx() {
+    bbnote "Deploying intermediate product name list files from ${CYCLONEDX_TMP_WORK_DIR} to ${CYCLONEDX_WORK_DIR}"
+}
+python do_populate_cyclonedx_setscene() {
+    sstate_setscene(d)
+}
+
+do_populate_cyclonedx[cleandirs] = "${CYCLONEDX_WORK_DIR}"
+do_populate_cyclonedx[sstate-inputdirs] = "${CYCLONEDX_TMP_WORK_DIR}"
+do_populate_cyclonedx[sstate-outputdirs] = "${CYCLONEDX_WORK_DIR}"
+addtask do_populate_cyclonedx_setscene
+addtask do_populate_cyclonedx after do_cyclonedx_package_collect
+do_rootfs[recrdeptask] += "do_populate_cyclonedx"
 
 def read_json(path):
     import json
@@ -137,7 +134,8 @@ def generate_packages_list(products_names, version):
     version = version.split("+git")[0]
 
     # some packages have alternative names, so we split CVE_PRODUCT
-    for product in products_names.split():
+    # convert to set to avoid duplicates
+    for product in set(products_names.split()):
         # CVE_PRODUCT in recipes may include vendor information for CPE identifiers. If not,
         # use wildcard for vendor.
         if ":" in product:
@@ -158,7 +156,11 @@ def generate_packages_list(products_names, version):
         packages.append(pkg)
     return packages
 
-def append_to_vex_vulnerabilities(d, vex, cve, sbom_serial_number, bom_ref):
+def append_to_vex(d, cve, cves, bom_ref):
+    """
+    Collect CVE status information from within open embedded recipes and append to add to cve dictionary.
+    This could be backported CVE fixes or ignored CVEs.
+    """
     from oe.cve_check import decode_cve_status
 
     decoded_status = decode_cve_status(d, cve)
@@ -183,14 +185,113 @@ def append_to_vex_vulnerabilities(d, vex, cve, sbom_serial_number, bom_ref):
         detail_string += f"STATE: {decoded_status['detail']}\n"
     if decoded_status["description"]:
         detail_string += f"JUSTIFICATION: {decoded_status['description']}\n"
-    vex["vulnerabilities"].append({
+
+    cves.append({
         "id": cve,
         # vex documents require a valid source, see https://github.com/DependencyTrack/dependency-track/issues/2977
         # this should always be NVD for yocto CVEs.
         "source": {"name": "NVD", "url": f"https://nvd.nist.gov/vuln/detail/{cve}"},
         "analysis": {
             "state": vex_state,
-            "detail": detail_string
+            "detail": detail_string,
         },
-        "affects": [{"ref": f"urn:cdx:{sbom_serial_number}/1#{bom_ref}"}]
+        "affects": [{"ref": f"urn:cdx:{d.getVar('CYCLONEDX_SBOM_SERIAL_PLACEHOLDER')}/1#{bom_ref}"}]
     })
+    return
+
+python do_deploy_cyclonedx() {
+    """
+    Select CVE and package information and runtime packages and output them into a single export file.
+    """
+    from oe.rootfs import image_list_installed_packages
+    import uuid
+    from datetime import datetime, timezone
+    import os
+
+    timestamp = datetime.now(timezone.utc).isoformat()
+
+    # Generate unique serial numbers for sbom and vex document
+    sbom_serial_number = str(uuid.uuid4())
+    vex_serial_number = str(uuid.uuid4())
+
+    cyclonedx_work_dir_root = d.getVar("CYCLONEDX_WORK_DIR_ROOT")
+
+    # Generate sbom document header
+    bb.debug(2, f"Creating empty temporary sbom file with serial number {sbom_serial_number}")
+    sbom = {
+        "bomFormat": "CycloneDX",
+        "specVersion": "1.4",
+        "serialNumber": f"urn:uuid:{sbom_serial_number}",
+        "version": 1,
+        "metadata": {
+            "timestamp": timestamp,
+            "tools": [{"name": "yocto"}]
+        },
+        "components": []
+    }
+
+    # Generate vex document header
+    bb.debug(2, f"Creating empty temporary vex file with serial number {sbom_serial_number}")
+    vex = {
+        "bomFormat": "CycloneDX",
+        "specVersion": "1.4",
+        "serialNumber": f"urn:uuid:{vex_serial_number}",
+        "version": 1,
+        "metadata": {
+            "timestamp": timestamp,
+            "tools": [{"name": "yocto"}]
+        },
+        "vulnerabilities": []
+    }
+
+    # taken from https://github.com/yoctoproject/poky/blob/fec201518be3c35a9359ec8c37675a33e458b92d/meta/classes/cve-check.bbclass
+    # SPDX-License-Identifier: MIT
+    # Copyright OpenEmbedded Contributors
+    # Collect sbom data from runtime packages
+
+    recipes = set()
+    if d.getVar('CYCLONEDX_RUNTIME_PACKAGES_ONLY') == "1":
+        for pkg in list(image_list_installed_packages(d)):
+            pkg_info = os.path.join(d.getVar('PKGDATA_DIR'),
+                                    'runtime-reverse', pkg)
+            pkg_data = oe.packagedata.read_pkgdatafile(pkg_info)
+            recipes.add(pkg_data["PN"])
+    else:
+        recipes = {pn for pn in os.listdir(cyclonedx_work_dir_root) if os.path.isdir(os.path.join(cyclonedx_work_dir_root, pn))}
+
+    save_pn = d.getVar("PN")
+    for pkg in recipes:
+        # To be able to use the CYCLONEDX_WORK_DIR_PN_LIST variable we have to evaluate
+        # it with the different PN names set each time.
+        d.setVar("PN", pkg)
+
+        pn_list_filepath = d.getVar("CYCLONEDX_WORK_DIR_PN_LIST")
+
+        if not os.path.exists(pn_list_filepath):
+            continue
+
+        pn_list = read_json(pn_list_filepath)
+
+        for pn_pkg in pn_list["pkgs"]:
+            # Avoid multiple pkgs referencing the same cpe
+            for sbom_pkg in sbom["components"]:
+                if pn_pkg["cpe"] == sbom_pkg["cpe"]:
+                    break
+            else:
+                sbom["components"].append(pn_pkg)
+        for pn_cve in pn_list["cves"]:
+            pn_cve["affects"][0]["ref"] = pn_cve["affects"][0]["ref"].replace(
+                d.getVar('CYCLONEDX_SBOM_SERIAL_PLACEHOLDER'), sbom_serial_number)
+            vex["vulnerabilities"].append(pn_cve)
+
+    d.setVar("PN", save_pn)
+
+    write_json(d.getVar("CYCLONEDX_EXPORT_SBOM"), sbom)
+    write_json(d.getVar("CYCLONEDX_EXPORT_VEX"), vex)
+}
+do_deploy_cyclonedx[cleandirs] = "${CYCLONEDX_EXPORT_DIR}"
+
+# We use ROOTFS_POSTUNINSTALL_COMMAND to make sure this function runs exactly once
+# after the build process has been completed
+# see https://docs.yoctoproject.org/ref-manual/variables.html#term-ROOTFS_POSTUNINSTALL_COMMAND
+ROOTFS_POSTUNINSTALL_COMMAND =+ "do_deploy_cyclonedx; "

commit 8f53d15e531e8e3f7b430f1d163e3831e2b1c094

Fix cleaning of leftovers when previous builds
The cleandirs flag is only valid for bitbake tasks, and does nothing for
handler functions.

So we need to do the cleanup manually in the handler function instead.

In addition to the problems described in the comment before the function,
switching between different MACHINE configurations in the same build dir
was causing problems, such as this:

ERROR: mc:initramfs:perl-5.38.2-r0 do_populate_cyclonedx_setscene: Recipe perl is trying to install files into a shared area when those files already exist. The files and the manifests listing them are:
  /build/tmp-musl/cyclonedx/perl/pn-list.json
    (matched in manifest-cortexa53-crypto-perl.populate_cyclonedx)
Please adjust the recipes so only one recipe provides a given file.
WARNING: Logfile for failed setscene task is /build/tmp-musl/work/core2-64-oe-linux-musl/perl/5.38.2/temp/log.do_populate_cyclonedx_setscene.1512710
WARNING: Setscene task (mc:initramfs:/src/oe-core/meta/recipes-devtools/perl/perl_5.38.2.bb:do_populate_cyclonedx_setscene) failed with exit code '1' - real task will be run instead

Fixes: d3a78c04785e ("Limit SBOM content to packages added to the rootfs")
Signed-off-by: Esben Haabendal <esben@geanix.com>

classes/cyclonedx-export.bbclass
@@ -108,9 +108,13 @@ python () {
 # Clean out work folder to avoid leftovers from previous builds when including build-time package
 # information and a recipe was removed from the dependency list. (CYCLONEDX_RUNTIME_PACKAGES_ONLY set to 0)
 python clean_cyclonedx_work_folder() {
-    bb.note(f"Cleaning cyclonedx work folder {d.getVar('CYCLONEDX_WORK_DIR_ROOT')}")
+    import shutil
+    cyclonedx_work_dir_root = d.getVar('CYCLONEDX_WORK_DIR_ROOT')
+    bb.note(f"Cleaning cyclonedx work folder {cyclonedx_work_dir_root}")
+    if os.path.exists(cyclonedx_work_dir_root):
+        shutil.rmtree(cyclonedx_work_dir_root)
+    bb.utils.mkdirhier(cyclonedx_work_dir_root)
 }
-clean_cyclonedx_work_folder[cleandirs] = "${CYCLONEDX_WORK_DIR_ROOT}"
 addhandler clean_cyclonedx_work_folder
 clean_cyclonedx_work_folder[eventmask] = "bb.event.BuildStarted"
 